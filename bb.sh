d078a40b66ff784a7172e1124320e16e2c9d1dd5
KonstantinosTourtsakis <p19tour@ionio.gr>
2023-04-03 20:44:46 +0300

    Added global URL

--- bb.sh
+++ bb.sh
@@ -24,7 +24,7 @@ global_variables() {
     # The typical subtitle for each blog
     global_description="A blog about the SW course!"
     # The public base URL for this blog
-    global_url="http://example.com/blog"
+    global_url="https://konstantinostourtsakis.github.io/sw-static-blog"
 
     # Your name
     global_author="Konstantinos Tourtsakis"
